96d244c4397696144c81b1edc820aad3e614883c
diegoQuinas <diegoQuinas@users.noreply.github.com>
2026-07-16 18:15:25 +0000

    deploy: be51444717e673e58af6f712281c2991a0e9f94a

---
+++ assets/diegoquinas-rs_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wbg_jsowner_free: (a: number, b: number) => void;
+export const wasm_bindgen_b1767ba5621cefcf___convert__closures_____invoke___alloc_a941a2cfab7efbc4___vec__Vec_wasm_bindgen_b1767ba5621cefcf___JsValue___wasm_bindgen_b1767ba5621cefcf___JsValue______true_: (a: number, b: number, c: number, d: number, e: any) => void;
+export const wasm_bindgen_b1767ba5621cefcf___convert__closures_____invoke___wasm_bindgen_b1767ba5621cefcf___JsValue__core_9b3796e30d99ddb7___result__Result_____wasm_bindgen_b1767ba5621cefcf___JsError___true_: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen_b1767ba5621cefcf___convert__closures_____invoke___web_sys_9004a2fae1743f87___features__gen_Event__Event______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_b1767ba5621cefcf___convert__closures________invoke___web_sys_9004a2fae1743f87___features__gen_Event__Event______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_b1767ba5621cefcf___convert__closures_____invoke_______true_: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
